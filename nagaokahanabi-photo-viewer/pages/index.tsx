618a41427d1e548ea429fbcc17bf600ca3a3a593
KeitoAbe <63314807+KeitoAbe@users.noreply.github.com>
2022-11-22 15:44:32 +0900

    画像を拡大 (#76)
    * 画像を拡大
    
    * コメント対応
    
    * 条件式 && JSXに書き換え

--- nagaokahanabi-photo-viewer/pages/index.tsx
+++ nagaokahanabi-photo-viewer/pages/index.tsx
@@ -1,24 +1,42 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import React, { useState } from "react";
 
 export default function Home() {
+  const [imgNumber, setImgNumber] = useState(0);
+
   return (
-    <div className={styles.container}>
-      <div className={styles.imgContainer}>
-        {(() => {
-          const list = [];
-          for (let i = 1; i < 64; i++) {
-            list.push(
-              <img
-                src={`/original/img-nagaokamatsuri-${i}.jpg`}
-                className={styles.img}
-                key={i}
-              />
-            );
-          }
-          return <div>{list}</div>;
-        })()}
+    <div>
+      {imgNumber !== 0 && (
+        <div className={styles.imgScaleContainer}>
+          <div
+            className={styles.closeButton}
+            onClick={() => setImgNumber(0)}
+          ></div>
+          <img
+            className={styles.scaleImg}
+            src={`/original/img-nagaokamatsuri-${imgNumber}.jpg`}
+          />
+        </div>
+      )}
+      <div className={styles.container}>
+        <div className={styles.imgContainer}>
+          {(() => {
+            const list = [];
+            for (let i = 1; i < 64; i++) {
+              list.push(
+                <img
+                  src={`/original/img-nagaokamatsuri-${i}.jpg`}
+                  className={styles.img}
+                  key={i}
+                  onClick={() => setImgNumber(i)}
+                />
+              );
+            }
+            return <div>{list}</div>;
+          })()}
+        </div>
       </div>
     </div>
   );
