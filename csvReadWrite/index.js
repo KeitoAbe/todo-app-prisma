ba44d8a69fa7c5331ca78c8a83ca65f28240f58f
KeitoAbe <63314807+KeitoAbe@users.noreply.github.com>
2022-12-09 17:09:21 +0900

    try...catchを追加 (#97)
    * try...catchを追加
    
    * console.errorに変更

--- csvReadWrite/index.js
+++ csvReadWrite/index.js
@@ -4,19 +4,23 @@ const { stringify } = require("csv-stringify/sync");
 const filePath = process.argv[2];
 const inputString = process.argv[3];
 
-if (inputString === undefined) {
-  const inputData = fs.readFileSync(filePath, { encoding: "utf8" });
-  console.log(inputData);
-} else {
-  const inputData = fs.readFileSync(filePath, { encoding: "utf8" });
-  const parsedData = parse(inputData, { columns: false });
-  const inputRecordArray = inputString.split(",");
-  if (parsedData[0].length !== inputRecordArray.length){
-    console.log("レコードのカラム数が一致しません");
-    process.exit(1);
-  }
+try {
+  if (inputString === undefined) {
+    const inputData = fs.readFileSync(filePath, { encoding: "utf8" });
+    console.log(inputData);
+  } else {
+    const inputData = fs.readFileSync(filePath, { encoding: "utf8" });
+    const parsedData = parse(inputData, { columns: false });
+    const inputRecordArray = inputString.split(",");
+    if (parsedData[0].length !== inputRecordArray.length) {
+      console.log("レコードのカラム数が一致しません");
+      process.exit(1);
+    }
     parsedData.push(inputRecordArray);
     const outputData = stringify(parsedData, { header: false });
     fs.writeFileSync(filePath, outputData, { encoding: "utf8" });
-    console.log(outputData); 
+    console.log(outputData);
+  }
+} catch (error) {
+  console.error(error.message);
 }
