afa5e06207eb851015fd441615e2ffe9ab7dfeab
KeitoAbe <63314807+KeitoAbe@users.noreply.github.com>
2023-01-11 15:41:41 +0900

    Promise形式に書き換え

--- generateObjectArrayFromCSV/index.js
+++ generateObjectArrayFromCSV/index.js
@@ -1,25 +1,26 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 const filePath = process.argv[2];
 
-fs.readFile(filePath, "utf8", (err, data) => {
-  if (err) {
+fs.readFile(filePath, "utf8")
+  .then((data) => {
+    const inputDataArray = data.split("\n");
+    const keyArray = inputDataArray[0].split(",");
+    const objectArray = [];
+    inputDataArray.slice(1).forEach((value) => {
+      const columnValueArray = value.split(",");
+      if (keyArray.length < columnValueArray.length) {
+        console.error("CSVファイルのカラム数が正しくありません");
+        process.exit(1);
+      }
+      const object = {};
+      keyArray.forEach((keyValue, keyIndex) => {
+        object[keyValue] = columnValueArray[keyIndex];
+      });
+      objectArray.push(object);
+    });
+    console.log(objectArray);
+  })
+  .catch((err) => {
     console.error(err);
     process.exit(1);
-  }
-  const inputDataArray = data.split("\n");
-  const keyArray = inputDataArray[0].split(",");
-  const objectArray = [];
-  inputDataArray.slice(1).forEach((value) => {
-    const columnValueArray = value.split(",");
-    if (keyArray.length < columnValueArray.length) {
-      console.error("CSVファイルのカラム数が正しくありません");
-      process.exit(1);
-    }
-    const object = {};
-    keyArray.forEach((keyValue, keyIndex) => {
-      object[keyValue] = columnValueArray[keyIndex];
-    });
-    objectArray.push(object);
   });
-  console.log(objectArray);
-});
