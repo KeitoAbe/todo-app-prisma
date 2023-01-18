4482d3035db9c8aae95a85c3355956e432b38ce1
KeitoAbe <63314807+KeitoAbe@users.noreply.github.com>
2023-01-18 16:23:30 +0900

    通信失敗時のエラー処理を追加 (#115)
    * 通信失敗時のエラー処理を追加
    
    * 不要なコードの削除
    
    * catchの引数を削除
    
    * urlを修正

--- ramen-api/pages/index.tsx
+++ ramen-api/pages/index.tsx
@@ -3,25 +3,48 @@ import Head from "next/head";
 
 const searchRamen = async () => {
   const response = await fetch("https://ramen-api.dev/shops/yoshimuraya");
+  if (!response.ok) {
+    console.error("response.ok:", response.ok);
+    console.error("esponse.status:", response.status);
+    console.error("esponse.statusText:", response.statusText);
+    throw new Error(response.statusText);
+  }
   const data = await response.json();
   return data;
 };
 
 export default function Home(Ramen: any) {
-  return (
-    <div>
-      <h1>{Ramen.name}</h1>
-      <img src={Ramen.img} alt={Ramen.name} width="500px" />
-    </div>
-  );
+  if (Ramen.error) {
+    return (
+      <div>
+        <p>通信に失敗しました</p>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>{Ramen.name}</h1>
+        <img src={Ramen.img} alt={Ramen.name} width="500px" />
+      </div>
+    );
+  }
 }
 
 export const getServerSideProps = async () => {
-  const res = await searchRamen();
-  return {
-    props: {
-      name: res.shop.name,
-      img: res.shop.photos[0].url,
-    },
-  };
+  try {
+    const res = await searchRamen();
+    return {
+      props: {
+        name: res.shop.name,
+        img: res.shop.photos[0].url,
+        error: false,
+      },
+    };
+  } catch {
+    return {
+      props: {
+        error: true,
+      },
+    };
+  }
 };
