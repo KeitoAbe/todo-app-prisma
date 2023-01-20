d4da0f4ef338a5f0e834bffa455b58e740c4337b
KeitoAbe <63314807+KeitoAbe@users.noreply.github.com>
2023-01-20 14:53:42 +0900

    useSWRに書き換え (#116)

--- ramen-api/pages/index.tsx
+++ ramen-api/pages/index.tsx
@@ -1,50 +1,21 @@
 /* eslint-disable @next/next/no-img-element */
 import Head from "next/head";
+import useSWR from "swr";
 
-const searchRamen = async () => {
-  const response = await fetch("https://ramen-api.dev/shops/yoshimuraya");
-  if (!response.ok) {
-    console.error("response.ok:", response.ok);
-    console.error("esponse.status:", response.status);
-    console.error("esponse.statusText:", response.statusText);
-    throw new Error(response.statusText);
-  }
-  const data = await response.json();
-  return data;
-};
+export default function Home() {
+  const fetcher = (url: string): Promise<any> =>
+    fetch(url).then((res) => res.json());
+  const { data, error, isLoading } = useSWR(
+    "https://ramen-api.dev/shops/yoshimuraya",
+    fetcher
+  );
 
-export default function Home(Ramen: any) {
-  if (Ramen.error) {
-    return (
-      <div>
-        <p>通信に失敗しました</p>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <h1>{Ramen.name}</h1>
-        <img src={Ramen.img} alt={Ramen.name} width="500px" />
-      </div>
-    );
-  }
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+  return (
+    <div>
+      <h1>{data.shop.name}</h1>
+      <img src={data.shop.photos[0].url} alt={data.shop.name} width="500px" />
+    </div>
+  );
 }
-
-export const getServerSideProps = async () => {
-  try {
-    const res = await searchRamen();
-    return {
-      props: {
-        name: res.shop.name,
-        img: res.shop.photos[0].url,
-        error: false,
-      },
-    };
-  } catch {
-    return {
-      props: {
-        error: true,
-      },
-    };
-  }
-};
