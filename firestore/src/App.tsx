c4f2452e6465cf0cba04180be694f32a54fb736d
KeitoAbe <63314807+KeitoAbe@users.noreply.github.com>
2023-02-10 14:58:24 +0900

    swrでFirestoreから取得 (#133)
    * swrでFirestoreから取得
    
    * 不要な記述を削除
    
    * keyをindexに変更

--- firestore/src/App.tsx
+++ firestore/src/App.tsx
@@ -1,22 +1,24 @@
-import { useState, useEffect } from "react";
 import "./App.css";
 import { db } from "../firebase";
 import { collection, getDocs, addDoc } from "firebase/firestore";
+import useSWR from "swr";
 
 function App() {
-  const [users, setUsers] = useState<any>([]);
-  useEffect(() => {
-    (async () => {
-      const querySnapshot = await getDocs(collection(db, "users"));
-      const nameArray = querySnapshot.docs.map((doc) => doc.data().name);
-      setUsers(nameArray);
-    })();
-  }, []);
+  const fetcher = async () => {
+    const querySnapshot = await getDocs(collection(db, "users"));
+    const nameArray = querySnapshot.docs.map((doc) => doc.data().name);
+    return nameArray;
+  };
+
+  const { data, error, isLoading } = useSWR("users", fetcher);
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading || !data) return <div>loading...</div>;
 
   return (
     <div className="App">
-      {users.map((user: string) => {
-        return <p>{user}</p>;
+      {data.map((name: string, index: number) => {
+        return <p key={index}>{name}</p>;
       })}
     </div>
   );
