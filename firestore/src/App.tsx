32e2599a2f0867622396f07b35f1a77fed56e1b3
KeitoAbe <63314807+KeitoAbe@users.noreply.github.com>
2023-02-16 14:30:02 +0900

    Google認証の追加、取得するコレクションの変更 (#135)
    * Google認証の追加、取得するコレクションの変更
    
    * スキーマの記述位置を変更
    
    * signInをhooksで扱うライブラリを導入、不要なexportを削除

--- firestore/src/App.tsx
+++ firestore/src/App.tsx
@@ -1,23 +1,38 @@
 import "./App.css";
-import { db } from "../firebase";
-import { collection, getDocs, addDoc } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 import useSWR from "swr";
 import { z } from "zod";
-
-const namesSchema = z.array(z.string());
+import { auth } from "../firebase";
+import { useAuthState, useSignInWithGoogle } from "react-firebase-hooks/auth";
 
 function App() {
+  const [user] = useAuthState(auth);
+  return <div>{user ? <GetName /> : <SignInButton />}</div>;
+}
+
+export default App;
+
+function SignInButton() {
+  const [signInWithGoogle] = useSignInWithGoogle(auth);
+  return (
+    <button onClick={() => signInWithGoogle}>
+      <p>Googleでサインイン</p>
+    </button>
+  );
+}
+
+function GetName() {
+  const db = getFirestore();
+  const namesSchema = z.array(z.string());
   const fetcher = async () => {
-    const querySnapshot = await getDocs(collection(db, "users"));
+    const querySnapshot = await getDocs(collection(db, "keitoAbe"));
     const nameArray = querySnapshot.docs.map((doc) => doc.data().name);
     return namesSchema.parse(nameArray);
   };
-
   const { data, error, isLoading } = useSWR("users", fetcher);
-
   if (error) return <div>failed to load</div>;
   if (isLoading || !data) return <div>loading...</div>;
-
   return (
     <div className="App">
       {data.map((name, index) => {
@@ -26,5 +41,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
