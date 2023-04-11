0ccddc47ea7e8595f3c143f24dd2e08a7cdb5e52
KeitoAbe <63314807+KeitoAbe@users.noreply.github.com>
2023-04-11 14:19:57 +0900

    プロジェクトを作成 (#152)
    * プロジェクトを作成
    
    * App.tsx、App.cssを削除
    
    * Appコンポーネントを削除

---
+++ thinking-in-react/src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+function FilterableProductTable() {
+  return <div>FilterableProductTable</div>;
+}
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <FilterableProductTable />
+  </React.StrictMode>
+);
