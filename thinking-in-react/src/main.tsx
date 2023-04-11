56cf69eebd1162a98950f306a5602172caf9f206
KeitoAbe <63314807+KeitoAbe@users.noreply.github.com>
2023-04-11 16:08:22 +0900

    Keito/static version (#154)
    * 静的なバージョンを作成
    
    * 不要なCSSを削除
    
    * 不要なclassNameを削除
    
    * 不要なセレクタを削除

--- thinking-in-react/src/main.tsx
+++ thinking-in-react/src/main.tsx
@@ -1,9 +1,59 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import styles from "./index.module.css";
 
 function FilterableProductTable() {
-  return <div>FilterableProductTable</div>;
+  return (
+    <div>
+      <div>
+        <input type="text" placeholder="Search..." />
+      </div>
+      <label>
+        <input type="checkbox" /> Only show products in stock
+      </label>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th colSpan={2}>Fruits</th>
+          </tr>
+          <tr>
+            <td>Apple</td>
+            <td>$1</td>
+          </tr>
+          <tr>
+            <td>Dragonfruit</td>
+            <td>$1</td>
+          </tr>
+          <tr>
+            <td className={styles.soldOut}>Passionfruit</td>
+            <td>$2</td>
+          </tr>
+
+          <tr>
+            <th colSpan={2}>Vegetables</th>
+          </tr>
+          <tr>
+            <td>Spinach</td>
+            <td>$2</td>
+          </tr>
+          <tr>
+            <td className={styles.soldOut}>Pumpkin</td>
+            <td>$4</td>
+          </tr>
+          <tr>
+            <td>Peas</td>
+            <td>$1</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
