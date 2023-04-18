a8d61a1820893ea5a8a41317897c75e69770f10f
KeitoAbe <63314807+KeitoAbe@users.noreply.github.com>
2023-04-18 11:50:36 +0900

    Storybookにインタラクションテストを導入 (#172)
    * Storybookにインタラクションテストを導入
    
    * 既存のstoriesファイルに配置、コメントでグループ分け

--- thinking-in-react/src/components/FilterableProductTable.stories.tsx
+++ thinking-in-react/src/components/FilterableProductTable.stories.tsx
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import FilterableProductTable from "./FilterableProductTable";
+import { userEvent, within } from "@storybook/testing-library";
+import { expect } from "@storybook/jest";
 
 const PRODUCTS = [
   {
@@ -29,4 +31,36 @@ type Story = StoryObj<typeof FilterableProductTable>;
 
 export const Primary: Story = {
   render: () => <FilterableProductTable products={PRODUCTS} />,
+};
+
+export const filterText: Story = {
+  render: () => <FilterableProductTable products={PRODUCTS} />,
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    await userEvent.type(canvas.getByRole("textbox"), "apple");
+    //入力された文字列を含む商品は表示される
+    await expect(canvas.getByText("Apple")).toBeInTheDocument();
+    //入力された文字列を含まない商品は表示されない
+    await expect(canvas.queryByText("Dragonfruit")).not.toBeInTheDocument();
+    await expect(canvas.queryByText("Passionfruit")).not.toBeInTheDocument();
+    await expect(canvas.queryByText("Spinach")).not.toBeInTheDocument();
+    await expect(canvas.queryByText("Pumpkin")).not.toBeInTheDocument();
+    await expect(canvas.queryByText("Peas")).not.toBeInTheDocument();
+  },
+};
+
+export const isStockOnly: Story = {
+  render: () => <FilterableProductTable products={PRODUCTS} />,
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    await userEvent.type(canvas.getByRole("checkbox"), "click");
+    // 在庫がある商品は表示される
+    await expect(canvas.getByText("Apple")).toBeInTheDocument();
+    await expect(canvas.getByText("Dragonfruit")).toBeInTheDocument();
+    await expect(canvas.getByText("Spinach")).toBeInTheDocument();
+    await expect(canvas.getByText("Peas")).toBeInTheDocument();
+    // 在庫がない商品は表示されない
+    await expect(canvas.queryByText("Passionfruit")).not.toBeInTheDocument();
+    await expect(canvas.queryByText("Pumpkin")).not.toBeInTheDocument();
+  },
 };
