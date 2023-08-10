1752b6b9bc867ec2f8ac01d4952fdc0e1ce91f88
KeitoAbe <63314807+KeitoAbe@users.noreply.github.com>
2023-08-10 15:26:33 +0900

    編集ボタン押下時にフォームにフォーカスを当てる

--- src/app/TodoListItem.tsx
+++ src/app/TodoListItem.tsx
@@ -72,6 +72,7 @@ export default function TodoList({
                   type="text"
                   value={editText}
                   onChange={(e) => setEditText(e.target.value)}
+                  autoFocus
                 />
               ) : (
                 todo.text
