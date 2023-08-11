ea908ad508ebb7c4478805e5b1bc727e14938bf5
KeitoAbe <63314807+KeitoAbe@users.noreply.github.com>
2023-08-11 16:39:37 +0900

    Todoリスト取得失敗時にエラーメッセージを表示

--- src/app/TodoList.tsx
+++ src/app/TodoList.tsx
@@ -2,12 +2,16 @@ import TodoListItem from "./TodoListItem";
 import { getTodoList } from "./actions";
 
 export default async function TodoList() {
-  const todoList = await getTodoList();
-  return (
-    <ul>
-      {todoList.map((todo) => (
-        <TodoListItem key={todo.id} todo={todo} />
-      ))}
-    </ul>
-  );
+  try {
+    const todoList = await getTodoList();
+    return (
+      <ul>
+        {todoList.map((todo) => (
+          <TodoListItem key={todo.id} todo={todo} />
+        ))}
+      </ul>
+    );
+  } catch (e) {
+    return <div>Todoリストの取得に失敗しました</div>;
+  }
 }
